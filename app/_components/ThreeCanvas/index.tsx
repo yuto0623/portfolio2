9480b89548d2b319c3205001c8152393bcd722ef
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-04 16:28:52 +0900

    test

--- app/_components/ThreeCanvas/index.tsx
+++ app/_components/ThreeCanvas/index.tsx
@@ -33,17 +33,19 @@ export default function ThreeCanvas() {
 
 		console.log(theme);
 		console.log(resolvedTheme);
-
-		if (bgRef.current) {
-			gsap.to(bgRef.current, {
-				duration: 0.3,
-				r: currentTheme === "dark" ? 0 : 1,
-				g: currentTheme === "dark" ? 0 : 1,
-				b: currentTheme === "dark" ? 0 : 1,
-			});
-		} else {
-			console.log("bgRef.current is null");
-		}
+		document.addEventListener("DOMContentLoaded", () => {
+			console.log("DOMContentLoaded");
+			if (bgRef.current) {
+				gsap.to(bgRef.current, {
+					duration: 0.3,
+					r: currentTheme === "dark" ? 0 : 1,
+					g: currentTheme === "dark" ? 0 : 1,
+					b: currentTheme === "dark" ? 0 : 1,
+				});
+			} else {
+				console.log("bgRef.current is null");
+			}
+		});
 	}, [theme, resolvedTheme]);
 
 	return (

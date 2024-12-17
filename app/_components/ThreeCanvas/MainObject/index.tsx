4b89ad4f1f8270d19a3d2e2a19bbb8c18816c67f
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-17 16:18:46 +0900

    MainObjectコンポーネント: ウィンドウサイズに応じたメッシュのスケーリング処理を追加

--- app/_components/ThreeCanvas/MainObject/index.tsx
+++ app/_components/ThreeCanvas/MainObject/index.tsx
@@ -1,10 +1,12 @@
+import { getWindowSize } from "@/app/hooks/GetWindowSize";
 import { ContactShadows, MeshTransmissionMaterial } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Mesh } from "three";
 
 export default function MainObject({ page }: { page: number }) {
 	const meshRefs = useRef<Mesh[]>([]);
+	const { height, width } = getWindowSize();
 
 	useFrame(() => {
 		meshRefs.current.forEach((mesh, index) => {
@@ -12,7 +14,7 @@ export default function MainObject({ page }: { page: number }) {
 				mesh.rotateX(0.005);
 				mesh.rotateZ(0.005);
 			}
-
+			//ページが切り替わるとオブジェクトも切り替わる(透明度の変化)
 			if (mesh?.material && "opacity" in mesh.material) {
 				if (index === 0 && !page) {
 					mesh.material.opacity += (1 - mesh.material.opacity) * 0.05;
@@ -23,6 +25,18 @@ export default function MainObject({ page }: { page: number }) {
 				}
 				mesh.material.needsUpdate = true;
 				mesh.material.transparent = true;
+			}
+
+			if (mesh?.material) {
+				if (width < 768 && mesh.geometry.type === "TorusKnotGeometry") {
+					mesh.scale.set(0.5, 0.5, 0.5);
+				} else if (width < 768 && mesh.geometry.type !== "TorusKnotGeometry") {
+					mesh.scale.set(0.7, 0.7, 0.7);
+				} else if (width >= 768 && mesh.geometry.type === "TorusKnotGeometry") {
+					mesh.scale.set(0.7, 0.7, 0.7);
+				} else {
+					mesh.scale.set(1, 1, 1);
+				}
 			}
 		});
 	});

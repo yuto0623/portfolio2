d89195d8379cdd8ac58da6333e870488d04e8dd4
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-17 13:32:52 +0900

    MainSliderコンポーネント: スライダーにスクロールインジケーターを追加し、react-iconsを更新

--- app/_components/MainSlider/index.tsx
+++ app/_components/MainSlider/index.tsx
@@ -6,6 +6,7 @@ import "swiper/css";
 import { useTheme } from "next-themes";
 import { parseAsInteger, useQueryState } from "nuqs";
 import { useEffect } from "react";
+import { HiOutlineArrowLongDown } from "react-icons/hi2";
 import { Mousewheel, Pagination } from "swiper/modules";
 import CustomPagination from "./CustomPagination";
 
@@ -70,8 +71,19 @@ export default function MainSlider() {
 	return (
 		<>
 			<Swiper {...swiperProps}>
-				<SwiperSlide>
+				<SwiperSlide className="relative">
 					<p>1Page</p>
+					<div
+						className={`absolute left-5 bottom-1/2 translate-y-1/2 [writing-mode:vertical-rl] text-xl
+							transition-all 
+							 ${page === 1 ? "opacity-100 duration-[2000ms]" : "opacity-0 -translate-x-full duration-300"}`}
+					>
+						Scroll
+						<span className="absolute top-0 left-0 h-[150%] w-0.5 bg-black" />
+						<span className="absolute -bottom-[25px] left-0 h-5 w-5 animate-bounce">
+							<HiOutlineArrowLongDown />
+						</span>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide>
 					<p>2Page</p>

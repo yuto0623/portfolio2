15dc48609bd526e8a82adbb23d5b8dac676f0fde
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-17 16:34:02 +0900

    MainSliderコンポーネント: カスタムページネーションのスタイルを調整し、レスポンシブ対応を強化

--- app/_components/MainSlider/CustomPagination/index.tsx
+++ app/_components/MainSlider/CustomPagination/index.tsx
@@ -5,15 +5,15 @@ const pageList = ["Top", "Works", "About", "Contact"];
 export default function CustomPagination({ page }: { page: number }) {
 	const swiper = useSwiper();
 	return (
-		<div className="fixed top-1/2 -translate-y-1/2 right-4 h-[60dvh] z-20 flex flex-col pr-3 justify-center">
+		<div className="fixed bottom-2 right-10 z-20 flex flex-col pr-3 justify-center md:h-[60dvh] md:right-4 md:top-1/2 md:bottom-0 md:-translate-y-1/2">
 			{/* <span className="h-full w-[1px] bg-black absolute right-0 top-0 rounded-full" /> */}
-			<ul className="text-right flex flex-col gap-4">
+			<ul className="text-right flex flex-row gap-4 md:flex-col">
 				{pageList.map((item, index) => (
 					<li
 						onClick={() => swiper.slideTo(index)}
 						onKeyDown={() => swiper.slideTo(index)}
 						key={item}
-						className={`relative transition-all duration-300 cursor-pointer ${index + 1 === page ? "text-4xl underline" : ""}`}
+						className={`flex items-end relative transition-all duration-300 cursor-pointer md:block ${index + 1 === page ? "text-4xl underline" : ""}`}
 					>
 						{item}
 					</li>

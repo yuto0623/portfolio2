fc82fcf1cdf7ee20f3c764670c2fd1ddc770861e
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-23 11:28:23 +0900

    RootLayoutコンポーネントのHTML要素にsuppressHydrationWarning属性を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja">
+		<html lang="ja" suppressHydrationWarning>
 			<body className={`${MontserratSubrayadaRegular.variable} antialiased`}>
 				<ThemeProvider>
 					<NuqsAdapter>

eef90523de8cf1ab85fd4453efda35c499beba4d
yuto0623 <yuto.ryr0623@gmail.com>
2024-12-23 10:15:28 +0900

    microcms-js-sdkを追加し、microCMSクライアントを作成するライブラリを実装

---
+++ app/_libs/microCMS.ts
@@ -0,0 +1,7 @@
+// libs/client.js
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+	serviceDomain: "service-domain",
+	apiKey: process.env.API_KEY || "",
+});

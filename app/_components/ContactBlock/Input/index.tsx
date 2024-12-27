b66494e56459707419f29c77ba0cf71d09f3dc1a
yuto <yuto.ryr0623@gmail.com>
2024-12-27 21:14:23 +0900

    ContactBlockコンポーネントにInputコンポーネントのテーマ対応を追加し、Buttonコンポーネントを新規作成

--- app/_components/ContactBlock/Input/index.tsx
+++ app/_components/ContactBlock/Input/index.tsx
@@ -1,8 +1,18 @@
+"use client";
+import { useCustomTheme } from "@/app/hooks/useCustomTheme";
+
 type InputProps = {
 	type: string;
 	placeholder: string;
 };
 
 export default function Input({ type, placeholder }: InputProps) {
-	return <input type={type} placeholder={placeholder} />;
+	const isTheme = useCustomTheme();
+	return (
+		<input
+			type={type}
+			placeholder={placeholder}
+			className={`py-3 px-6 rounded-2xl transition-all duration-300 ${isTheme === "dark" ? "shadow-[5px_5px_10px_#252525,-5px_-5px_10px_#1a1a1a;]" : " shadow-[5px_5px_10px_#d6d6d6,-5px_-5px_10px_#ffffff;]"}`}
+		/>
+	);
 }
--- app/_components/ContactBlock/index.tsx
+++ app/_components/ContactBlock/index.tsx
@@ -3,7 +3,7 @@ import Input from "./Input";
 export default function ContactBlock() {
 	return (
 		<div>
-			<h1>ContactBlock</h1>
+			<h2 className="text-3xl mb-6">CONTACT</h2>
 			<form className="flex flex-col gap-4">
 				<Input type="text" placeholder="Name" />
 				<Input type="email" placeholder="Email" />

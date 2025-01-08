23c17e311f02b7fcd0ea858f632e3c258714685a
yuto <yuto.ryr0623@gmail.com>
2025-01-08 20:56:30 +0900

    AboutBlockコンポーネントの会社名の表現を一般化しました

--- app/_components/AboutBlock/index.tsx
+++ app/_components/AboutBlock/index.tsx
@@ -11,7 +11,7 @@ export default function AboutBlock() {
 					広島コンピュータ専門学校でHTML、CSS、Photoshop、Illustratorの基礎を学び、以降独学でJavascript、PHP、React、Next.jsを学んでいます。
 				</p>
 				<p>その他にも3DCG関係やスマホアプリ開発等にも興味があります。</p>
-				<p>現在、ユウベル株式会社でWEB関係を担当。</p>
+				<p>現在の会社ではWEB関係を担当。</p>
 				<p>趣味はプログラミング、ゲーム、ツーリング、ドライブ。</p>
 			</div>
 		</div>

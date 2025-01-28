56142d801c244f99a284e85f292d78dd776710d7
yuto0623 <yuto.ryr0623@gmail.com>
2025-01-28 11:05:24 +0900

    Update AboutBlock

--- app/_components/AboutBlock/index.tsx
+++ app/_components/AboutBlock/index.tsx
@@ -10,9 +10,9 @@ export default function AboutBlock() {
 				<p>
 					広島コンピュータ専門学校でHTML、CSS、Photoshop、Illustratorの基礎を学び、以降独学でJavascript、PHP、React、Next.jsを学んでいます。
 				</p>
-				<p>その他にも3DCG関係やスマホアプリ開発等にも興味があります。</p>
+				<p>その他にも3DCG関係やスマホアプリ開発、写真等にも興味があります。</p>
 				<p>現在の会社ではWEB関係を担当。</p>
-				<p>趣味はプログラミング、ゲーム、ツーリング、ドライブ。</p>
+				<p>趣味はプログラミング、ゲーム、ツーリング、ドライブ、写真。</p>
 			</div>
 		</div>
 	);

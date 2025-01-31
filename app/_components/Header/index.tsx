2b2cdc47fa3f0800ca5cff5b288f7190f6d19e39
yuto0623 <yuto.ryr0623@gmail.com>
2025-01-31 15:26:45 +0900

    Enhance header and pagination styles with accent color and improved character rendering

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
 	return (
 		<header className="fixed top-0 left-0 w-full h-32 px-[5%] flex items-center text-3xl">
-			<h1>YUTO SHINTANI</h1>
+			<h1>
+				YU<span className="text-accent">T</span>O SHINTANI
+			</h1>
 		</header>
 	);
 }
--- app/_components/MainSlider/CustomPagination/index.tsx
+++ app/_components/MainSlider/CustomPagination/index.tsx
@@ -6,18 +6,24 @@ export default function CustomPagination({ page }: { page: number }) {
 	const swiper = useSwiper();
 	return (
 		<div className="fixed bottom-14 right-1 z-20 flex flex-col pr-3 justify-center md:h-[60dvh] md:right-4 md:top-1/2 md:bottom-0 md:-translate-y-1/2">
-			{/* <span className="h-full w-[1px] bg-black absolute right-0 top-0 rounded-full" /> */}
 			<ul className="text-right flex flex-col gap-4">
 				{pageList.map((item, index) => (
 					<li
 						onClick={() => swiper.slideTo(index)}
 						onKeyDown={() => swiper.slideTo(index)}
 						key={item}
-						className={`flex items-start relative transition-all duration-300 cursor-pointer [writing-mode:vertical-rl]
-						
-							 md:[writing-mode:horizontal-tb] md:block ${index + 1 === page ? "text-2xl md:text-4xl" : ""}`}
+						className={`flex items-start relative transition-all duration-300 cursor-pointer [writing-mode:vertical-rl] md:[writing-mode:horizontal-tb] md:block ${
+							index + 1 === page ? "text-2xl md:text-4xl" : ""
+						}`}
 					>
-						{item}
+						{item.split("").map((char, charIndex) => (
+							<span
+								key={char + charIndex.toString()}
+								className={`${charIndex === 0 && index + 1 === page ? "text-accent" : ""}`}
+							>
+								{char}
+							</span>
+						))}
 					</li>
 				))}
 			</ul>

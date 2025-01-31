a3ae6f510498748edb4d2fa049507e90a7743d52
yuto0623 <yuto.ryr0623@gmail.com>
2025-01-31 14:46:32 +0900

    Update API route to fetch all contents from the works endpoint

--- app/api/works/route.ts
+++ app/api/works/route.ts
@@ -2,6 +2,7 @@ import { client } from "@/app/_libs/microCMS";
 import { NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse> {
-	const data = await client.getList({ endpoint: "works" });
-	return new NextResponse(JSON.stringify(data), { status: 200 });
+	// const data = await client.getList({ endpoint: "works" });
+	const data = await client.getAllContents({ endpoint: "works" });
+	return new NextResponse(JSON.stringify({ contents: data }), { status: 200 });
 }

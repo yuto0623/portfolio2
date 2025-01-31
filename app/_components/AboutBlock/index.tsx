732d833d37b3ba24bd53d143ad6718b3192c684a
yuto0623 <yuto.ryr0623@gmail.com>
2025-01-31 11:17:10 +0900

    Refactor AboutBlock component text for clarity and conciseness

--- app/_components/AboutBlock/index.tsx
+++ app/_components/AboutBlock/index.tsx
@@ -22,8 +22,8 @@ export default function AboutBlock() {
 				<p>
 					広島コンピュータ専門学校でHTML、CSS、Photoshop、Illustratorの基礎を学び、以降独学でJavascript、PHP、React、Next.jsを学んでいます。
 				</p>
-				<p>その他にも3DCG関係やスマホアプリ開発、写真等にも興味があります。</p>
-				<p>現在の会社ではWEB関係を担当。</p>
+				<p>その他にも3DCG関係やアプリ開発、写真等にも興味があります。</p>
+				<p>現在、会社でWEB関係を担当。</p>
 				<p>趣味はプログラミング、ゲーム、ツーリング、ドライブ、写真。</p>
 			</div>
 		</GlassContainer>
